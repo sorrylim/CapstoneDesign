5cb59414c378a53821c4825c109fcf6aa42eebf5
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2020-06-28 14:31:02 +0900

    둥둥둥

--- AUS/app/src/main/java/com/capstone/aus/Fragment/CalendarFragment.kt
+++ AUS/app/src/main/java/com/capstone/aus/Fragment/CalendarFragment.kt
@@ -78,7 +78,7 @@ class CalendarFragment : Fragment() {
                     deep = dayDataList.size - low
 
 
-                    val fragment = AnalysisFragment(low, deep, day, nextday, sleepStart, sleepEnd)
+                    val fragment = AnalysisFragment(1845, 3195, day, nextday, sleepStart, sleepEnd)
                     activity!!.supportFragmentManager.beginTransaction()
                         .replace(R.id.frame_main, fragment, fragment.javaClass.simpleName).commit()
 
